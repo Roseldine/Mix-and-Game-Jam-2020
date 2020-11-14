6e79445e9650ceefecca5ae2228f3acd02ec89d7
BenWahBalls <74424360+BenWahBalls@users.noreply.github.com>
2020-11-14 19:28:26 +0000

    rere

---
+++ Assets/Resources/Scripts/Zeca UI Script/EnemiesNumber.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemiesNumber : MonoBehaviour
+{     
+    public int  Score;
+    public Text ScoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Entities"))
+        {
+            Debug.Log("layer");
+            AddScore();
+            }
+     
+        
+    }
+
+    void AddScore()
+    {
+        Score++;
+        ScoreText.text = Score.ToString();
+    }
+}
